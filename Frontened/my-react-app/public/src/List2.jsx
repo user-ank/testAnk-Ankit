f349ad2a03f5ea9c912b22185ea156c3d30aa016
ANKIT <kumarankit0327@gmail.com>
2023-03-09 22:51:06 +0530

    edited

---
+++ Frontened/my-react-app/public/src/List2.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+ function List2 ()
+ {
+ return (
+
+    
+<ol>
+<li>GOT</li>
+<li>DARK</li>
+<li>SEX EDUCATION</li>
+
+</ol>
+
+
+ );
+ }
+
+ export default List2;
---
+++ Frontened/my-react-app/public/src/Para.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+ function Para ()
+ {
+ return <p1>okay this is paragraph from the component</p1>
+
+ }
+
+ export default Para;
